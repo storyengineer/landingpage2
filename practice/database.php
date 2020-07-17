bd7b6bf54ad54b0bcf3ab56c149bf2a5d5f0a1b6
storyengineer <66221766+storyengineer@users.noreply.github.com>
2020-07-17 03:59:44 -0700

    Update database.php

--- practice/database.php
+++ practice/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("localhost","root","","testDB");
+$db = mysqli_connect("x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","t8vj5y5lz3ad3cx1","","y2vksti735mq9c58");
 
 if(!$db)
 {
